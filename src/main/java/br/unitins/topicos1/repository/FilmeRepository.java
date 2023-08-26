58a057654f811f98a0f3f7f0ed6689841118d024
Sabrina <sabrinamalaquias@unitins.br>
2023-08-26 00:32:02 -0300

    commit inicial

---
+++ src/main/java/br/unitins/topicos1/repository/FilmeRepository.java
@@ -0,0 +1,17 @@
+package br.unitins.topicos1.repository;
+
+import java.util.List;
+
+import br.unitins.topicos1.model.Filme;
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+import jakarta.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class FilmeRepository implements PanacheRepository<Filme>
+{
+    public List<Filme> findByNome(String nome)
+    {
+        return find("UPPER(nome) LIKE UPPER (?1)", "%"+nome+"%").list();
+        
+    }
+}
---
+++ src/main/resources/import.sql
@@ -0,0 +1,17 @@
+-- This file allow to write SQL commands that will be emitted in test and dev.
+-- The commands are commented as their support depends of the database
+-- insert into myentity (id, field) values(1, 'field-1');
+-- insert into myentity (id, field) values(2, 'field-2');
+-- insert into myentity (id, field) values(3, 'field-3');
+-- alter sequence myentity_seq restart with 4;
+
+
+INSERT INTO filme ( nome, genero, ator, idioma, legenda, duracao, idade) VALUES ('Harry Potter e a Pedra Filosofal', 'Fantasia', 'Daniel Radcliffe', 'Ingles', 'Portugues', '2h32min', 'Livre para todos os publicos');
+INSERT INTO filme ( nome, genero, ator, idioma, legenda, duracao, idade) VALUES ('Harry Potter e a Camara Secreta', 'Fantasia', 'Daniel Radcliffe', 'Ingles', 'Portugues', '2h30min', 'Livre para todos os publicos');
+INSERT INTO filme ( nome, genero, ator, idioma, legenda, duracao, idade) VALUES ('Harry Potter e o Prizoneiro de Azkaban', 'Fantasia', 'Daniel Radcliffe', 'Ingles', 'Portugues', '2h20min', '12 anos');
+INSERT INTO filme ( nome, genero, ator, idioma, legenda, duracao, idade) VALUES ('Harry Potter e o Calice de Fogo', 'Fantasia', 'Daniel Radcliffe', 'Ingles', 'Portugues', '2h35min', '12 anos');
+INSERT INTO filme ( nome, genero, ator, idioma, legenda, duracao, idade) VALUES ('Harry Potter e a Ordem da Fenix', 'Fantasia', 'Daniel Radcliffe', 'Ingles', 'Portugues', '2h18min', '12 anos');
+INSERT INTO filme ( nome, genero, ator, idioma, legenda, duracao, idade) VALUES ('Harry Potter e o Enigma do Principe', 'Fantasia', 'Daniel Radcliffe', 'Ingles', 'Portugues', '2h32m', '14 anos');
+INSERT INTO filme ( nome, genero, ator, idioma, legenda, duracao, idade) VALUES ('Harry Potter e as Reliquias da Morte: parte 1', 'Fantasia', 'Daniel Radcliffe', 'Ingles', 'Portugues', '2h25min', '14 anos');
+INSERT INTO filme ( nome, genero, ator, idioma, legenda, duracao, idade) VALUES ('Harry Potter e as Reliquias da Morte: parte 2', 'Fantasia', 'Daniel Radcliffe', 'Ingles', 'Portugues', '2h10min', '14 anos');
+
